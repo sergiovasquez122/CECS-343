812b3c9bf47e41bf99a523c15613a38e2f39b129
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2020-12-11 16:48:11 -0800

    Update app.js

--- app.js
+++ app.js
@@ -133,7 +133,6 @@ let load_labeling = function(source_directory){
     }
     return hash_table;
 }
-
 /**
  * writes a new labeling of a manifest_file to the .labeling.txt file
  * @param source_directory
@@ -154,7 +153,6 @@ let label_command = function(source_directory, manifest_or_label, new_labeling){
         throw err;
     }
 }
-
 /**
  * give the absolute path to a manifest file/label recreate a project tree in the desired target directory
  * @param manifest_file_name_absolute_path the absolute path of a manifest file/label
@@ -193,7 +191,6 @@ let checkout = function(manifest_file_name_absolute_path, target_directory, comm
         }
     }
 }
-
 /**
  * Does a depth-first search traversal computing the check sum of files, the relative path from the source and the byte size of the files
  * and then creating an artificial id for the file and copying it into the target directory.
@@ -234,22 +231,15 @@ let clone = function (dir, source_directory, target_directory, manifest_file_nam
                         const file_extension = path.posix.extname(itemPath);
                         const target_path = path.posix.join(target_directory, artificial_id_of_file + file_extension);
                         const data_to_save = artificial_id_of_file + file_extension + " " + relative_path_from_source_to_item_directory + "/" + item + '\n';
-                        fs.appendFile(path.posix.join(source_directory, manifest_file_name), data_to_save, function (err){
-                            if(err) console.log(err);
-                        });
-                        fs.appendFile(path.posix.join(target_directory, manifest_file_name), data_to_save, function (err){
-                            if(err) console.log(err);
-                        });
-                        fs.copyFile(itemPath, target_path, function (err){
-                            if(err) console.log(err);
-                        });
+                        fs.appendFileSync(path.posix.join(source_directory, manifest_file_name), data_to_save);
+                        fs.appendFileSync(path.posix.join(target_directory, manifest_file_name), data_to_save);
+                        fs.copyFileSync(itemPath, target_path);
                     });
                 }
             });
         });
     });
 };
-
 /**
  * Given two directories, clones the content of the source directory into the target directory with the content file name converted into artificial id's
  * @param source_directory
@@ -259,22 +249,17 @@ let clone = function (dir, source_directory, target_directory, manifest_file_nam
 let clone_directory = function (source_directory, target_directory, command){
     mkdirRecursive(target_directory);
     let manifest_file_name = ".man-1.txt";
-    fs.writeFile(path.posix.join(source_directory, manifest_file_name), getTimeStamp() + '\n'+ command + '\n', function (err){
-        if(err) console.log(err);
-    });
-    fs.writeFile(path.posix.join(target_directory, manifest_file_name), getTimeStamp() + '\n'+ command +'\n', function (err){
-        if(err) console.log(err);
-    });
+    fs.writeFileSync(path.posix.join(source_directory, manifest_file_name), getTimeStamp() + '\n'+ command + '\n');
+    fs.writeFileSync(path.posix.join(target_directory, manifest_file_name), getTimeStamp() + '\n'+ command +'\n');
     clone(source_directory, source_directory, target_directory, manifest_file_name);
 }
-
 /**
  * find the most current manifest file
  * @param source_directory the directory to search for the most current manifest file
  * @returns {number} the most current version of the manifest as a number
  */
 let find_largest_manifest_file_value = function (source_directory){
-    let options = {'cwd' : path.resolve(source_directory) , 'dot' : true, 'root' : path.resolve(source_directory)};
+    let options = {'cwd' : path.posix.join(source_directory) , 'dot' : true, 'root' : path.posix.join(source_directory)};
     let files = glob.sync(".man-+([0-9]).txt", options).toString().split(",");
     let largest_integer = 1;
     for(let i = 0;i < files.length;i++){
@@ -285,7 +270,6 @@ let find_largest_manifest_file_value = function (source_directory){
     }
     return largest_integer;
 }
-
 /**
  * store the latest version of user project in specified target directory
  * @param source_directory the source directory the user would like to 'check-in'
@@ -294,47 +278,44 @@ let find_largest_manifest_file_value = function (source_directory){
  */
 let check_in = function(source_directory, target_directory, command){
     let most_current_manifest_value = find_largest_manifest_file_value(source_directory);
-    mkdirRecursive(target_directory);
     // increment by one to note that we now have one version larger
     most_current_manifest_value++;
     let manifest_file_name = ".man-" + most_current_manifest_value + ".txt";
-    fs.writeFile(path.posix.join(source_directory, manifest_file_name), getTimeStamp() + '\n'+ command + '\n', function (err){
-        if(err) console.log(err);
-    });
-    fs.writeFile(path.posix.join(target_directory, manifest_file_name), getTimeStamp() + '\n'+ command +'\n', function (err){
-        if(err) console.log(err);
-    });
+    fs.writeFileSync(path.posix.join(source_directory, manifest_file_name), getTimeStamp() + '\n'+ command + '\n');
+    fs.writeFileSync(path.posix.join(target_directory, manifest_file_name), getTimeStamp() + '\n'+ command +'\n');
     clone(source_directory, source_directory, target_directory, manifest_file_name);
 }
-
 /**
- * Given a string do a particular function execution and output the results of doing said function
+ * Given a string do a particular function execution
  * @param command the string that was inputted by a user
  */
 function executeCmd(command){
     let component = command.split(' ');
     switch(component[0]){
         case "createRepo" :
-            clone_directory(component[1], component[2], command) //comp1 == src dir && comp2 == repo dir
+            clone_directory(component[1], component[2], command)
             return (Promise.resolve("Repo Created"))
         case "checkin":
-            check_in(component[1], component[2], command) //comp1 == src dir && comp2 == target dir
+            check_in(component[1], component[2], command)
             return (Promise.resolve("Successfully Checked-In " + component[1] + " to " + component[2]))
         case "checkout":
-            checkout(component[1],component[2], command) //comp1 == mani-file dir && comp2 == target dir
+            checkout(component[1],component[2], command)
             return (Promise.resolve("Successfully Checked " + component[1] + " out to " + component[2]))
         case "label" :
-            label_command(component[1],component[2],component[3]) //comp1 == src dir && comp2 == mani-file OR label && comp3 == new label
+            label_command(component[1],component[2],component[3])
             return (Promise.resolve("Successfuly added the label " + component[3] + " to the file " + component[2]))
         case "list":
-            return (Promise.resolve(list(component[1]))) //comp1 == repo directory
+            return (Promise.resolve(list(component[1])))
         case "mergeOut":
-            merge_out(component[1],component[2], component[3], command) //comp1 == repo dir && comp 2 == mani-file && comp 3 == proj tree
+            merge_out(component[1],component[2], component[3], command) // comp1 == repo dir && comp 2 == mani-file && comp 3 == proj tree
+            //^^name may change, just what I put in for now
             return(Promise.resolve((component[3] + " ready to merge to " + component[1] + "\n Please manually merge any 3-way merges before calling mergeIn.")))
         case "mergeIn":
             merge_in(component[1], component[2], command); //comp 1 == repo dir  && comp 2 == proj tree
+            //mergeIn func would just call check_in [I'm assuming] so we can change it to check_in func if that's the case
             return (Promise.resolve("Successfully Checked-In " + component[2] + " to " + component[1]))
 
+    }
 }
 
 //Driver Code
@@ -356,6 +337,187 @@ app.post('/get_user_request', (req, res, next) => {
     ;
     
 });
+/**
+ * Given an array, return a new array with the content in lexicographical ordering
+ * @param A containing string elements
+ * @return array with the elements of the original array in lexicographical order
+ */
+let sort = function(A){
+    let copy  = A.slice();
+    copy.sort()
+    return copy;
+}
+/**
+ * Given an array, return a new array with the content in reverse order
+ * @param A containing string elements
+ * @returns array with the elements of the original array in lexicographical order
+ */
+let reverse = function(A){
+    let copy = A.slice();
+    copy.reverse();
+    return copy;
+}
+/**
+ * Given a source directory and a manifest file, collect all the relative paths of a file from the source directory, first element in thearray is
+ * absolute path of the source directory
+ * @param source_directory
+ * @param manifest_file
+ * @return a hashtable that contains the paths as keys and the artid as values
+ */
+let get_files = function (source_directory, manifest_file){
+    let file_path = path.posix.join(source_directory, manifest_file);
+    let hash_table = new Map();
+    if(fs.existsSync(file_path)){
+        let lrs = new line_reader(file_path);
+        let lines = lrs.toLines();
+        for(let i = 2;i < lines.length;i++){
+            let artid_and_file_path = lines[i].split(" ");
+            hash_table.set(artid_and_file_path[1], artid_and_file_path[0]);
+        }
+    }
+    return hash_table;
+}
+/**
+ * Find the last common path among the repo and the project tree
+ * @param repo_directory
+ * @param repo_manifest_file_name
+ * @param project_tree_directory
+ * @param tree_manifest_file_name
+ * @return absolute path of the grandma
+ */
+let find_grandma = function (repo_directory, repo_manifest_file_name, project_tree_directory, tree_manifest_file_name){
+    let source_paths = traverse_to_root(repo_directory, repo_manifest_file_name);
+    let target_paths = traverse_to_root(project_tree_directory, tree_manifest_file_name);
+    let source_hash_table = new Set(source_paths);
+    target_paths = reverse(target_paths);
+    let last_common_path = "";
+    for(let i = 0;i < target_paths.length;i++) {
+        if (source_hash_table.has(target_paths[i])) {
+            last_common_path = target_paths[i];
+        }
+    }
+    return last_common_path;
+}
+/**
+ * Given the source directory and manifest file, return the path it takes to get to the root as an array
+ * @param source_directory
+ * @param manifest_file
+ * @return root to node path
+ */
+let traverse_to_root = function(source_directory, manifest_file){
+    let file_path = path.posix.join(source_directory, manifest_file);
+    let result = [];
+    while(fs.existsSync(file_path)){
+        let lrs = new line_reader(file_path);
+        let lines = lrs.toLines();
+        let command = lines[1].split(" ");
+        if(command[0] !== 'createRepo'){
+            result.push(command[2]);
+            source_directory = command[1];
+            let most_current_manifest_value = find_largest_manifest_file_value(source_directory);
+            manifest_file = ".man-" + most_current_manifest_value + ".txt";
+            file_path = path.posix.join(source_directory, manifest_file);
+        } else {
+            result.push(command[2]);
+            break;
+        }
+    }
+    return result;
+}
+/**
+ * Given two string, compare them
+ * @param a
+ * @param b
+ * @return -1 if a < b, 0 if a == b, 1 if a > b
+ */
+function strcmp(a, b)
+{
+    return (a<b?-1:(a>b?1:0));
+}
+/**
+ * Merge the content of a repo and project tree, doing a three way merge if necessary
+ * @param repo_directory
+ * @param manifest_file
+ * @param project_tree_directory
+ * @param command
+ */
+let merge_out = function (repo_directory, manifest_file, project_tree_directory, command){
+    // add project tree content into repo
+    check_in(project_tree_directory, repo_directory, command)
+    let source_files_and_artid = get_files(repo_directory, manifest_file);
+    let latest_manifest_for_tree = ".man-" + find_largest_manifest_file_value(project_tree_directory) + ".txt";
+    let target_files_and_artid = get_files(project_tree_directory, latest_manifest_for_tree);
+    let source_files = Array.from(source_files_and_artid.keys());
+    let target_files = Array.from(target_files_and_artid.keys());
+    source_files = sort(source_files);
+    target_files = sort(target_files);
+    // ignore the repo directories
+    let i = 0;
+    let j = 0;
+    while(i < source_files.length && j < target_files.length){
+        if(strcmp(source_files[i], target_files[j]) < 0){
+            // case 1: the source wins, copy source to project tree
+            let file_directory = path.posix.dirname(source_files[i]);
+            // make the directory recursively if not in target
+            mkdirRecursive(path.posix.join(project_tree_directory, file_directory));
+            // copy the content of the repo directory to the project tree
+            fs.copyFileSync(path.posix.join(repo_directory, source_files_and_artid.get(source_files[i])),
+            path.posix.join(project_tree_directory, source_files[i])
+            );
+            i = i + 1;
+        } else if(strcmp(source_files[i], target_files[j]) > 0){
+            // case 2: target wins, don't need to add
+            j = j + 1;
+        } else if(strcmp(source_files[i], target_files[j]) === 0
+                    && source_files_and_artid.get(source_files[i]) === target_files_and_artid.get(target_files[j])){
+            // case 3: target wins, don't need to add
+            i = i + 1;
+            j = j + 1;
+        } else {
+            // case 4: target and source have files but they differ, add grandma and let user take cover of three way merge
+            // rename target to have _mt append before the extension
+            let file_name_without_extension_of_target = path.posix.parse(target_files[j]).name;
+            let extension_of_target = path.posix.parse(target_files[j]).ext;
+            fs.renameSync(path.posix.join(project_tree_directory, target_files[j]),
+                path.posix.join(project_tree_directory, file_name_without_extension_of_target + "_MT" + extension_of_target));
+            // copy the content of source to project tree and append _mr to end before extension
+            let file_name_without_extension_of_source = path.posix.parse(target_files[j]).name;
+            let extension_of_source = path.posix.parse(target_files[j]).ext;
+            fs.copyFileSync(path.posix.join(repo_directory, source_files_and_artid.get(source_files[i])),
+                path.posix.join(project_tree_directory, file_name_without_extension_of_source + "_MR" + extension_of_source));
+            // copy the content of grandma and append MG to it
+            let grandma_path = find_grandma(repo_directory, manifest_file, project_tree_directory, latest_manifest_for_tree);
+            let grandma_manifest = ".man-" + find_largest_manifest_file_value(grandma_path) + ".txt";
+            let grandma_files_and_artids = get_files(grandma_path, grandma_manifest);
+            if(grandma_files_and_artids.has(target_files[j])){
+                // copy grandma's content of the file from the repo directory to the target with the _MG added to it
+                fs.copyFileSync(path.posix.join(repo_directory, grandma_files_and_artids.get(target_files[j]))
+                                , path.posix.join(project_tree_directory, file_name_without_extension_of_source + "_MG" + extension_of_source));
+            }
+        }
+    }
+    // source has more content then target
+    while(i < source_files_and_artid.size()){
+        // case 1: the source wins, copy source to project tree
+        let file_directory = path.posix.dirname(source_files[i]);
+        // make the directory recursively if not in target
+        mkdirRecursive(path.posix.join(project_tree_directory, file_directory));
+        // copy the content of the repo directory to the project tree
+        fs.copyFileSync(path.posix.join(repo_directory, source_files_and_artid.get(source_files[i])),
+            path.posix.join(project_tree_directory, source_files[i])
+        );
+        i = i + 1;
+    }
+}
+/**
+ * Given a repo_directory and a project_tree_directory, merge their content after three-way merge
+ * @param repo_directory
+ * @param project_tree_directory
+ * @param command
+ */
+let merge_in = function (repo_directory, project_tree_directory, command){
+    check_in(project_tree_directory, repo_directory, command);
+}
 
 //Handles serving the landing page 
 app.get('/', function(req,res){
